8da3f63d6ec4ec3ecaa4350dde179b3461f97072
Tyler M <tmart234@gmail.com>
2023-10-30 00:13:45 -0600

    adding a search bar to main view

--- WW-app/RiverListView.swift
+++ WW-app/RiverListView.swift
@@ -8,29 +8,48 @@ import SwiftUI
 
 struct RiverListView: View {
     @EnvironmentObject var riverDataModel: RiverDataModel
+    @State private var searchTerm: String = ""
+
+    // This computed property will filter the rivers based on the search term.
+    var filteredRivers: [USGSRiverData] {
+        if searchTerm.isEmpty {
+            return riverDataModel.rivers
+        } else {
+            return riverDataModel.rivers.filter { $0.stationName.lowercased().contains(searchTerm.lowercased()) }
+        }
+    }
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(riverDataModel.rivers.indices, id: \.self) { index in
-                    NavigationLink(destination: RiverDetailView(river: riverDataModel.rivers[index])) {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(riverDataModel.rivers[index].stationName)
-                                    .font(.headline)
-                                Text("\(riverDataModel.rivers[index].siteNumber)")
-                                    .font(.subheadline)
+            VStack {
+                // Search bar
+                TextField("Search by station name...", text: $searchTerm)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+
+                List {
+                    ForEach(filteredRivers.indices, id: \.self) { index in
+                        NavigationLink(destination: RiverDetailView(river: filteredRivers[index])) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(filteredRivers[index].stationName)
+                                        .font(.headline)
+                                    Text("\(filteredRivers[index].siteNumber)")
+                                        .font(.subheadline)
+                                }
+                                Spacer()
                             }
-                            Spacer()
                         }
-                    }
-                    .swipeActions {
-                        Button(action: {
-                            riverDataModel.toggleFavorite(at: index)
-                        }) {
-                            Label(riverDataModel.rivers[index].isFavorite ? "Unfavorite" : "Favorite", systemImage: riverDataModel.rivers[index].isFavorite ? "star.slash.fill" : "star.fill")
+                        .swipeActions {
+                            Button(action: {
+                                riverDataModel.toggleFavorite(at: index) // Ensure this works with filtered rivers
+                            }) {
+                                Label(filteredRivers[index].isFavorite ? "Unfavorite" : "Favorite", systemImage: filteredRivers[index].isFavorite ? "star.slash.fill" : "star.fill")
+                            }
+                            .tint(filteredRivers[index].isFavorite ? .gray : .yellow)
                         }
-                        .tint(riverDataModel.rivers[index].isFavorite ? .gray : .yellow)
                     }
                 }
             }
@@ -38,7 +57,6 @@ struct RiverListView: View {
         }
     }
 }
-
 
 
 
