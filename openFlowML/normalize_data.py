340832de9eb3dbc1adc013746a6144862093db1b
Tyler M <tmart234@gmail.com>
2023-10-30 13:41:09 -0600

    Update normalize_data.py

--- openFlowML/normalize_data.py
+++ openFlowML/normalize_data.py
@@ -6,9 +6,15 @@ def normalize_temperature_data(input_file_path, output_file_path):
     # Read the CSV
     data = pd.read_csv(input_file_path)
 
-    # Ensure temperatures is a column in your CSV
-    scaler = StandardScaler()
-    data['temperatures'] = scaler.fit_transform(data['temperatures'].values.reshape(-1, 1))
+    # Ensure 'TMIN' and 'TMAX' columns are in your CSV
+    columns_to_normalize = ['TMIN', 'TMAX']
+
+    # Create a scaler for each column
+    scalers = {}
+    for column in columns_to_normalize:
+        scaler = StandardScaler()
+        data[column] = scaler.fit_transform(data[column].values.reshape(-1, 1))
+        scalers[column] = scaler
 
     # Save the normalized data
     data.to_csv(output_file_path, index=False)
@@ -21,3 +27,4 @@ if __name__ == "__main__":
     # Update the CSV_FILE_PATH to point to the normalized data for the artifact step
     with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
         env_file.write(f"CSV_FILE_PATH={OUTPUT_PATH}\n")
+
