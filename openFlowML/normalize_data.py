8c46137f57128dedef4caa4ed2e544c8aefcbdd5
Tyler M <tmart234@gmail.com>
2023-10-30 18:21:32 -0600

    Update normalize_data.py

--- openFlowML/normalize_data.py
+++ openFlowML/normalize_data.py
@@ -20,11 +20,6 @@ def normalize_temperature_data(input_file_path, output_file_path):
     data.to_csv(output_file_path, index=False)
 
 if __name__ == "__main__":
-    INPUT_PATH = os.getenv('CSV_FILE_PATH')  # Retrieve the path from the environment variable
-    OUTPUT_PATH = INPUT_PATH.replace('.csv', '_normalized.csv')  # Name the normalized CSV
-    normalize_temperature_data(INPUT_PATH, OUTPUT_PATH)
-    
-    # Update the CSV_FILE_PATH to point to the normalized data for the artifact step
-    with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
-        env_file.write(f"CSV_FILE_PATH={OUTPUT_PATH}\n")
-
+    INPUT_PATH = 'combined_data.csv'
+    OUTPUT_PATH = INPUT_PATH.replace('.csv', '_normalized.csv')
+    normalize_data(INPUT_PATH, OUTPUT_PATH)
