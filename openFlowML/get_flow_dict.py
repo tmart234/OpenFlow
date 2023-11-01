ba28192e9dfd474f57c1573a324041fa037e0f45
Tyler M <tmart234@gmail.com>
2023-11-01 13:01:21 -0600

    Update get_flow_dict.py

--- openFlowML/get_flow_dict.py
+++ openFlowML/get_flow_dict.py
@@ -1,6 +1,6 @@
 import requests
 import argparse
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def get_daily_flow_data(flow_site_id, start_date, end_date):
     url = f"https://nwis.waterservices.usgs.gov/nwis/iv/?sites={flow_site_id}&parameterCd=00060&startDT={start_date}&endDT={end_date}&siteStatus=all&format=rdb"
@@ -12,18 +12,24 @@ def get_daily_flow_data(flow_site_id, start_date, end_date):
     # Initialize a dictionary to store daily min and max flow values
     daily_flow_data = {}
 
-    # Skip lines
-    content = content[content.index("#\n")+3:]
+    # Find the start line that contains actual data headers (e.g., "agency_cd site_no ...")
+    for index, line in enumerate(content):
+        if line.startswith('agency_cd'):
+            start_line = index + 1
+            break
 
-    for line in content:
-        columns = line.split()
+    # Extract the actual data lines
+    data_lines = content[start_line:]
+
+    for line in data_lines:
+        columns = line.split('\t')
         if len(columns) >= 5:
-            datetime_str = f"{columns[2]} {columns[3]} {columns[4]}"
+            datetime_str = f"{columns[2]} {columns[3]}"
             try:
-                dt = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M %Z")
-                formatted_datetime_str = dt.strftime("%Y-%m-%d %H:%M %Z")
+                dt = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
+                formatted_datetime_str = dt.strftime("%Y-%m-%d %H:%M")
                 
-                flow = columns[5]
+                flow = columns[4]
                 date_str = formatted_datetime_str.split()[0]
                 flow = float(flow)
                 
@@ -43,10 +49,10 @@ def main(args):
     daily_flow_data = get_daily_flow_data(args.flow_site_id, args.start_date, args.end_date)
 
     # Print the daily flow data
-    #for date, data in daily_flow_data.items():
-    #    min_flow = data["min"]
-    #    max_flow = data["max"]
-    #    print(f"Date: {date}, Min Flow: {min_flow}, Max Flow: {max_flow}")
+    for date, data in daily_flow_data.items():
+        min_flow = data["min"]
+        max_flow = data["max"]
+        print(f"Date: {date}, Min Flow: {min_flow}, Max Flow: {max_flow}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fetch daily flow data for a given USGS site.')
@@ -55,4 +61,3 @@ if __name__ == "__main__":
     parser.add_argument('end_date', type=str, help='End date in the format YYYY-MM-DD')
     args = parser.parse_args()
     main(args)
-
