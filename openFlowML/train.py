a09f17968e4502a97d03f331b3c22ecb2e73fe01
Tyler M <tmart234@gmail.com>
2023-11-10 13:38:08 -0700

    Update train.py

--- openFlowML/train.py
+++ openFlowML/train.py
@@ -26,43 +26,13 @@ def save_as_onnx(model, output_filename="lstm_model.onnx"):
 # make mix/max flow prediction for next 14 days, 
 # take in a window of 14 day future temps, 
 # and take in window of 60 days of past flow data
-def reshape_data_for_lstm(data, 
-                          historical_flow_timesteps=60, 
-                          forecast_temperature_timesteps=14, 
-                          forecast_flow_timesteps=14):
-    X, Y = [], []
-
-    # Ensure columns exist in data
-    required_columns = ["Min Flow", "Max Flow", "TMIN", "TMAX"]
-    for col in required_columns:
-        if col not in data.columns:
-            raise ValueError(f"Expected '{col}' column in the data.")
-    
-    total_required_days = historical_flow_timesteps + forecast_temperature_timesteps + forecast_flow_timesteps
-
-    for i in range(len(data) - total_required_days + 1):
-        # Extracting historical flow data and future temperature data
-        input_features = data.iloc[i:i+total_required_days][required_columns].values
-        X.append(input_features)
-
-        # Extracting the flow data (both Min and Max) for the next 14 days (the target)
-        future_min_flow = data.iloc[i+historical_flow_timesteps:i+historical_flow_timesteps+forecast_flow_timesteps]["Min Flow"].values
-        future_max_flow = data.iloc[i+historical_flow_timesteps:i+historical_flow_timesteps+forecast_flow_timesteps]["Max Flow"].values
-        
-        Y.append(np.stack([future_min_flow, future_max_flow], axis=-1))
-
-    return np.array(X), np.array(Y)
-
 # output a flattened prediction of shape [?, forecast_horizon * 2]
 # then immediately reshaped it to the desired [?, forecast_horizon, 2].
 # Reshape the data to be suitable for LSTM training
 def reshape_data_for_lstm(data, historical_flow_timesteps=60, forecast_temperature_timesteps=14, forecast_flow_timesteps=14):
     X, Y = [], []
-
-    # Include the date_normalized column in the required features
     required_columns = ["Min Flow", "Max Flow", "TMIN", "TMAX", "date_normalized"]
 
-    # Check if required columns exist
     for col in required_columns:
         if col not in data.columns:
             raise ValueError(f"Expected '{col}' column in the data.")
@@ -70,33 +40,26 @@ def reshape_data_for_lstm(data, historical_flow_timesteps=60, forecast_temperatu
     total_required_days = historical_flow_timesteps + forecast_temperature_timesteps
 
     for i in range(len(data) - total_required_days + 1):
-        # Extract features including the normalized date
         input_features = data.iloc[i:i+total_required_days][required_columns].values
         X.append(input_features)
 
-        # Extract future flow data for the target
-        future_min_flow = data.iloc[i+historical_flow_timesteps:i+historical_flow_timesteps+forecast_flow_timesteps]["Min Flow"].values
-        future_max_flow = data.iloc[i+historical_flow_timesteps:i+historical_flow_timesteps+forecast_flow_timesteps]["Max Flow"].values
-        Y.append(np.stack([future_min_flow, future_max_flow], axis=-1))
+        future_flow = data.iloc[i+historical_flow_timesteps:i+historical_flow_timesteps+forecast_flow_timesteps][["Min Flow", "Max Flow"]].values
+        Y.append(future_flow)
 
     return np.array(X), np.array(Y)
 
 # Build LSTM model
 def build_lstm_model(input_shape, forecast_horizon=14):
-    # Define input layer for historical data
     historical_input = Input(shape=input_shape, name='historical_input')
 
-    # Define LSTM layers with dropout for regularization
     lstm_out = LSTM(50, activation='relu', return_sequences=True)(historical_input)
     lstm_out = Dropout(0.2)(lstm_out)
     lstm_out = LSTM(50, activation='relu')(lstm_out)
     lstm_out = Dropout(0.2)(lstm_out)
 
-    # Dense output layer to predict future flow data
     dense_out = Dense(forecast_horizon * 2, activation="linear")(lstm_out)
     output = tf.keras.layers.Reshape((forecast_horizon, 2))(dense_out)
 
-    # Compile the model
     model = Sequential(inputs=historical_input, outputs=output)
     model.compile(optimizer='adam', loss='mse')
     
@@ -104,27 +67,20 @@ def build_lstm_model(input_shape, forecast_horizon=14):
 
 # Main function to execute the training process
 def main():
-    # Load and combine data (assuming this function returns a pandas DataFrame)
     data = combine_data.main()
     if isinstance(data, str):
         print("Error: Data is recognized as string. Expected pandas DataFrame.")
         exit(1)
 
-    # Reshape data for LSTM training
     X, Y = reshape_data_for_lstm(data)
 
-    # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
-    # Build LSTM model
     model = build_lstm_model(input_shape=(X_train.shape[1], X_train.shape[2]), forecast_horizon=14)
 
-    # Train the model
     model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
-    # Save the trained model
     save_as_h5(model)
-
 
 # Call main
 if __name__ == '__main__':
