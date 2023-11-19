1fd52905a088a417765025d7499f985c89961876
Tyler M <tmart234@gmail.com>
2023-11-19 16:53:50 -0700

    Update ml_utils.py

--- openFlowML/ml_utils.py
+++ openFlowML/ml_utils.py
@@ -2,9 +2,9 @@
 import coremltools as ct
 import sys
 
-def convert_model(h5_model_path, mlmodel_output_path, input_shape):
+def convert_model(model_path, mlmodel_output_path, input_shape):
     # Load the Keras model
-    model = ct.models.keras.load_model(h5_model_path)
+    model = ct.models.keras.load_model(model_path)
 
     # Convert the model to Core ML format
     mlmodel = ct.convert(model, inputs=[ct.TensorType(shape=input_shape)])
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python convert_to_coreml.py <h5_model_path> <mlmodel_output_path> <input_shape>")
     else:
-        h5_model_path = sys.argv[1]
+        model_path = sys.argv[1]
         mlmodel_output_path = sys.argv[2]
         input_shape = eval(sys.argv[3])  # Convert string input to tuple
         convert_model(h5_model_path, mlmodel_output_path, input_shape)
